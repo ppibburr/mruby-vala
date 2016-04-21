d2f3f93f6f92feffd78c6e612776313f8f37263c
ppibburr <tulnor33@gmail.com>
2016-04-20 23:16:44 -0400

    ...

--- mruby.sh
+++ mruby.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 mkdir -p vendor
-#rm -rf vendor
+rm -rf vendor
 mkdir -p vendor 
-cd vendor #&& git clone https://github.com/ppibburr/mruby-shared.git
+cd vendor && git clone https://github.com/ppibburr/mruby-shared.git
 cd mruby-shared
 cp ../../build_config.rb ./
-#CFLAGS+=-fPIC make
+CFLAGS+=-fPIC make
  
 cp libmruby.so /usr/lib/libmruby-shared.so
 cp ../../mruby.pc /usr/lib/pkgconfig/
